30626ab84cc8d830fd19f82c6fc5f048dfe33864
Christopher Robbertse <chris.robbertse@openbanking.org.uk>
2025-07-17 11:55:49 +0100

    remove redundant brackets and return statement in 1 line Lambdas

--- web/vue.config.js
+++ web/vue.config.js
@@ -56,13 +56,11 @@ module.exports = {
     // * https://cli.vuejs.org/guide/webpack.html#replacing-loaders-of-a-rule
     // * https://github.com/vuejs/vue-cli/issues/3215
 
-    config.module.rule('fonts').use('url-loader').tap((opts) => {
-      return Object.assign(opts, { limit: 0 });
-    });
+    config.module.rule('fonts').use('url-loader').tap((opts) =>
+        Object.assign(opts, { limit: 0 }));
 
-    config.module.rule('images').use('url-loader').tap((opts) => {
-      return Object.assign(opts, { limit: 0 });
-    });
+    config.module.rule('images').use('url-loader').tap((opts) =>
+        Object.assign(opts, { limit: 0 }));
 
     // Copy `ReDoc` (https://github.com/Rebilly/ReDoc) to `/static/redoc/bundles/redoc.standalone.js`
     // so that `pkg/server/swagger_handlers.go` can use it as the `RedocURL`.
